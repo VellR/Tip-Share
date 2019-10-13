2c0b6c28132462042284cb91ed3b5f64872fb69d
VellR <Vell.Romas@gmail.com>
2019-10-13 16:34:23 -0500

    Add files via upload

---
+++ sources/android/support/v7/recyclerview/R.java
@@ -0,0 +1,30 @@
+package android.support.v7.recyclerview;
+
+public final class R {
+
+    public static final class attr {
+        public static final int layoutManager = 2130772201;
+        public static final int reverseLayout = 2130772203;
+        public static final int spanCount = 2130772202;
+        public static final int stackFromEnd = 2130772204;
+    }
+
+    public static final class dimen {
+        public static final int item_touch_helper_max_drag_scroll_per_frame = 2131230835;
+        public static final int item_touch_helper_swipe_escape_max_velocity = 2131230836;
+        public static final int item_touch_helper_swipe_escape_velocity = 2131230837;
+    }
+
+    public static final class id {
+        public static final int item_touch_helper_previous_elevation = 2131558405;
+    }
+
+    public static final class styleable {
+        public static final int[] RecyclerView = {16842948, justthetip.ivellapplication.com.justthetip.R.attr.layoutManager, justthetip.ivellapplication.com.justthetip.R.attr.spanCount, justthetip.ivellapplication.com.justthetip.R.attr.reverseLayout, justthetip.ivellapplication.com.justthetip.R.attr.stackFromEnd};
+        public static final int RecyclerView_android_orientation = 0;
+        public static final int RecyclerView_layoutManager = 1;
+        public static final int RecyclerView_reverseLayout = 3;
+        public static final int RecyclerView_spanCount = 2;
+        public static final int RecyclerView_stackFromEnd = 4;
+    }
+}
